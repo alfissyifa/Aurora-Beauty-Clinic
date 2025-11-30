2cd8bcd5268d1d8a99f94d1a79e5cc098e48f9c6
DODI HERRYANTO <alfis22062009@gmail.com>
2025-11-30 14:25:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4820954319-9d576",
+  "appId": "1:544186171876:web:ecc6a22d9817973b7147f1",
+  "apiKey": "AIzaSyCLzbcN-Y0uw_OlupH3vOagZmCMsgXZmvE",
+  "authDomain": "studio-4820954319-9d576.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "544186171876"
+};

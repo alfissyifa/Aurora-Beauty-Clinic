3c09c9ace8ff9fe0b4cb9a35587cc079e5b92024
DODI HERRYANTO <alfis22062009@gmail.com>
2025-12-02 15:57:20 +0000

    menu layanan satu saja

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -43,7 +43,6 @@ export default function AdminLayout({
   ];
 
   const publicNavItems = [
-    { href: '/services', label: 'Layanan', icon: BriefcaseBusiness },
     { href: '/about', label: 'Tentang Kami', icon: Info },
     { href: '/contact', label: 'Kontak', icon: Phone },
   ];
